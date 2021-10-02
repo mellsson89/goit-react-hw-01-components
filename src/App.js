f1c368c6dc9df0a6dc909e4773932e07769c51b2
mellsson89 <mellsson@protonmail.com>
2021-10-02 13:11:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import Profile from './views/Profile';
 import Statistical from './views/Statistical';
 import FriendList from './views/FriendList';
 import TransactionHistory from './views/TransactionHistory';
-import style from './app.module.scss'
+import style from './app.module.scss';
+import route from "./route";
+
 
 
 const App = () =>  {
+
   return (
       <>
           <ul className={style.menu}>
               <li >
-                  <NavLink exact  to='/' className={style.menuListLink} activeClassName={style.menuListLink__active}>Профиль социальной сети</NavLink>
+                  <NavLink exact  to='/goit-react-hw-01-components' className={style.menuListLink} activeClassName={style.menuListLink__active}>Профиль социальной сети</NavLink>
               </li>
-              <li><NavLink  to='/statistical' className={style.menuListLink} activeClassName={style.menuListLink__active}>Секция статистики</NavLink></li>
-              <li><NavLink  to='/friends' className={style.menuListLink} activeClassName={style.menuListLink__active}>Список друзей</NavLink></li>
-              <li><NavLink  to='/transaction' className={style.menuListLink} activeClassName={style.menuListLink__active}>История транзакций</NavLink></li>
+              <li><NavLink  to='/goit-react-hw-01-components/statistical' className={style.menuListLink} activeClassName={style.menuListLink__active}>Секция статистики</NavLink></li>
+              <li><NavLink  to='/goit-react-hw-01-components/friends' className={style.menuListLink} activeClassName={style.menuListLink__active}>Список друзей</NavLink></li>
+              <li><NavLink  to='/goit-react-hw-01-components/transaction' className={style.menuListLink} activeClassName={style.menuListLink__active}>История транзакций</NavLink></li>
           </ul>
 
           <Switch>
-              <Route exact path='/' component={Profile}/>
-              <Route  path ='/statistical' component={Statistical}/>
-              <Route  path ='/friends' component={FriendList}/>
-              <Route  path ='/transaction' component={TransactionHistory}/>
+              <Route exact path={route.profile} component={Profile}/>
+              <Route  path ={route.statistical} component={Statistical}/>
+              <Route  path ={route.friend} component={FriendList}/>
+              <Route  path ={route.transactions} component={TransactionHistory}/>
               <Route component={Profile}/>
           </Switch>
       </>
---
+++ src/route.js
@@ -0,0 +1,6 @@
+export default {
+    profile:'/goit-react-hw-01-components',
+    statistical: '/goit-react-hw-01-components/statistical',
+    friend:'/goit-react-hw-01-components/friends',
+    transactions:'/goit-react-hw-01-components/transaction'
+}
